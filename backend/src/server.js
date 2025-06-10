0cf9a4413f7c95b389eefe6c1a7e67deefcf48a2
Asif <asifiqball5012@gmail.com>
2025-06-10 06:38:46 -0700

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,16 +5,27 @@ import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js"
 dotenv.config()
 import cors from "cors"
-
+import path from "path"
 const app = express()
-
+const __dirname = path.resolve()
 
 
 //middleware
 app.use(express.json())
-app.use(cors())
+if(process.env.NODE_ENV!=="production"){
+    app.use(cors())
+}
 app.use(rateLimiter)
 app.use("/api/notes",notesRoutes)
+
+
+if(process.env.NODE_ENV==="production"){
+    app.use("/api/notes",express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res)=>{
+   res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 
 const PORT = process.env.PORT || 5001
 
